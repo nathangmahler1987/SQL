07ce6bfb79c97d2502bc665e41b7d1b4d5988315
Nathan Mahler <nathangmahler@gmail.com>
2022-06-25 13:21:16 -0600

    Traffic Source Trending

---
+++ sqlexample02.sql
@@ -0,0 +1,11 @@
+-- Based on CONV. Rate we need to find gsearch nonbrand by week to see if bid changes have caused volume to drop
+USE MAVENFUZZYFACTORY;
+SELECT 
+	MIN(DATE(created_at)) as week_start_date,
+	COUNT( DISTINCT website_session_id) AS sessions
+FROM website_sessions
+WHERE utm_source = 'gsearch' AND utm_campaign = 'nonbrand' AND created_at <'2012-05-12'
+GROUP BY  YEAR(created_at), week(created_at)
+ORDER BY week_start_date
+
+
