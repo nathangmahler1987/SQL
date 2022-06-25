064a987edcbf040041ab8052588335d0c0580246
Nathan Mahler <nathangmahler@gmail.com>
2022-06-25 14:25:01 -0600

    Bid Optimization for Paid Traffic

--- sqlexample03.sql
+++ sqlexample03.sql
@@ -1,4 +1,14 @@
 USE MAVENFUZZYFACTORY;
 
-SELECT *
-FROM orders
+SELECT 
+	device_type,
+    COUNT( DISTINCT ws.website_session_id) AS sessions,
+    COUNT( distinct o.order_id) AS orders,
+	FORMAT(COUNT( DISTINCT o.order_id) / COUNT(distinct ws.website_session_id) * 100,2) AS conversion_rate
+FROM website_sessions AS ws 
+		LEFT OUTER JOIN orders AS o 
+			ON o.website_session_id = ws.website_session_id
+WHERE ws.created_at < '2012-05-11' AND utm_source = 'gsearch' AND utm_campaign = 'nonbrand'
+GROUP BY device_type
+
+
