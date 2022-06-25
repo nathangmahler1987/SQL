5ba6d4d466b223094271810395e3a628c88b4af2
nathangmahler1987 <73514763+nathangmahler1987@users.noreply.github.com>
2022-06-25 13:23:00 -0600

    Update sqlexample02.sql

--- sqlexample02.sql
+++ sqlexample02.sql
@@ -4,7 +4,7 @@ SELECT
 	MIN(DATE(created_at)) as week_start_date,
 	COUNT( DISTINCT website_session_id) AS sessions
 FROM website_sessions
-WHERE utm_source = 'gsearch' AND utm_campaign = 'nonbrand' AND created_at <'2012-05-12'
+WHERE utm_source = 'gsearch' AND utm_campaign = 'nonbrand' AND created_at <'2012-05-10'
 GROUP BY  YEAR(created_at), week(created_at)
 ORDER BY week_start_date
 
